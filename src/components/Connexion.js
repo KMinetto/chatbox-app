907810ba63ae89df246223417354039f41e4c4f9
Kilian MINETTO <killian.minetto@gmail.com>
2021-07-31 19:08:02 +0200

    Ajout des fonctionnalités de connexion avec React Router et Redirect

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 
 class Connexion extends Component {
+
+    state = {
+        pseudo: '',
+        goToChat: false
+    }
+
+    handleChange = (event) => {
+        const pseudo = event.target.value
+        this.setState({ pseudo })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({ goToChat: true })
+    }
+
     render() {
+        const userState = this.state
+
+        if (userState.goToChat) {
+            return <Redirect to={`/pseudo/${userState.pseudo}`} />
+        }
+
         return (
             <div className='connexionBox'>
-                <form className='connexion' action="">
+                <form
+                    className='connexion'
+                    onSubmit={this.handleSubmit}
+                >
                     <input
+                        value={userState.pseudo}
+                        onChange={this.handleChange}
                         type="text"
                         placeholder="Pseudo"
                         required
