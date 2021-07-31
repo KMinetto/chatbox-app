ba323cf672170537243190aa2dd3daa1ced7626c
Kilian MINETTO <killian.minetto@gmail.com>
2021-07-31 20:25:19 +0200

    Ajout des components Message et Formulaire

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './App.css';
 
+import Formulaire from "./components/Formulaire";
+import Message from "./components/Message";
+
 class App extends Component {
   render() {
     return (
         <div className='box'>
-            <h1>Bonjour</h1>
+            <div>
+                <div className="messages">
+                    <Message />
+                </div>
+                <Formulaire />
+            </div>
         </div>
     )
   }
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Message = () => {
+    return (
+        <div>
+            <p className="user-message">
+                Mon message
+            </p>
+        </div>
+    );
+};
+
+export default Message;
