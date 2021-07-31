e8c22479ff818acc037d4b352f93084f728e85d0
Kilian MINETTO <killian.minetto@gmail.com>
2021-07-31 18:52:41 +0200

    Ajout des différentes pages avec React Router

---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Error extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Il n'y a rien ici ! :)</h2>
+            </div>
+        );
+    }
+}
+
+export default Error;
